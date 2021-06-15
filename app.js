d2d1de5c8760344854c936aa5e30912d613b575e
neenreva <neenreva@gmail.com>
2021-06-15 15:37:34 -0400

    Basic layout

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-var jayBoy = 'Yeah man'
+const bookWrapper = document.querySelector('.bookWrapper');
+const form = document.querySelectorAll('.form');
+const submitInput = form[0].querySelector('input[type="submit"]')
+
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.info = function() { return `${title} by ${author}, ${pages}, ${read}` }
+}
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    let formData = new Book(form[0])
+    myLibrary.push(formData.getAll())
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    submitInput.addEventListener('click', addBookToLibrary, false)
+}, false)
+
+console.log(myLibrary);
