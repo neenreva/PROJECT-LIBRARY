a92008613f302dffa5dbafcf01544ba10807cb0d
Jimmy G <neenreva@gmail.com>
2021-08-30 17:42:18 -0400

    forms update

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const deleteBtn = document.querySelectorAll('#book-list .delete');
-    deleteBtn.forEach(function(btn) {
-        btn.addEventListener('click', function(e) {
-            const li = e.target.parentElement;
-            li.parentNode.removeChild(li);
-        })
+const list = document.querySelector('#book-list ul');
+list.addEventListener('click', function(e) {
+    if(e.target.className === 'delete') {
+        let li = e.target.parentElement;
+        list.removeChild(li);
+    }
 });
 
+const addForm = document.forms['add-book'];
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
 
+    //create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    bookName.classList.add('name');
+    const deleteBtn = document.createElement('span');
+    deleteBtn.classList.add('delete');
 
+    //add content
+    deleteBtn.textContent = ' delete';
+    bookName.textContent = value;
+
+    //append to dom
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
