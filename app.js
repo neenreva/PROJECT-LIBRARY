4607e6d1d67d57a9e0cf68274ae7a41dcd597431
Jimmy G <neenreva@gmail.com>
2021-08-30 15:20:40 -0400

    Reworking DOM from netninja tut

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-const bookWrapper = document.querySelector('.bookWrapper');
-const form = document.querySelectorAll('.form');
-const submitInput = form[0].querySelector('input[type="submit"]')
+const deleteBtn = document.querySelectorAll('#book-list .delete');
+    deleteBtn.forEach(function(btn) {
+        btn.addEventListener('click', function(e) {
+            const li = e.target.parentElement;
+            li.parentNode.removeChild(li);
+        })
+});
 
-let myLibrary = [];
 
-function Book(title, author, pages) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.info = function() { return `${title} by ${author}, ${pages}, ${read}` }
-}
 
-function addBookToLibrary(e) {
-    e.preventDefault();
-    let formData = new Book(form[0])
-    myLibrary.push(formData.getAll())
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    submitInput.addEventListener('click', addBookToLibrary, false)
-}, false)
-
-console.log(myLibrary);
